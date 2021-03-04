cc381afb681df6c9f6ca32ad36a5a2d41a996913
enriquelau <enriquelau@mac.com>
2021-03-04 15:19:42 -0600

    Script is working, just need to run sim tests.

--- end_purchase.py
+++ end_purchase.py
@@ -1,28 +1,5 @@
-import time
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-from urllib3.exceptions import NewConnectionError
-from webdriver_manager import driver
-from webdriver_manager.chrome import ChromeDriverManager
-#URL = 'https://www.speer.com/ammunition/handgun/lawman_handgun_training/19-53651.html'
-URL = 'https://www.speer.com/ammunition/handgun/lawman_handgun_training/19-53919.html'
-driver = webdriver.Chrome(ChromeDriverManager().install())
-quantity = "40"
-userEmail = "enriquelau@protonmail.com"
-password = "b%f)itq+wTZNRZN6=DT)zA:F"
-creditCard = "372653830681009"
-cardMonth = "08"
-cardYear = "2025"
-cardSecurityCode = "8636"
-
-def check_status(get_status):
-    status = get_status
-    while True:
-        if status == "Currently Unavailable":
-            time.sleep(5)
-            driver.refresh()
-        elif status == "Available":
+def end_purchase():
+        while True:
             try:  # Declines tracking consent
                 driver.find_element_by_class_name("decline.btn.btn-primary").click()
 
@@ -102,16 +79,3 @@ def check_status(get_status):
                 # placeOrder.send_keys(Keys.RETURN)
             except Exception:
                 continue
-
-def begin_purchase():
-    try:
-        driver.get(URL)
-        get_status = driver.find_element_by_class_name("availability.product-availability").text
-        check_status(get_status)
-    except (NoSuchElementException, NewConnectionError):
-        time.sleep(3)
-
-
-begin_purchase()
-
-
