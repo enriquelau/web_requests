cc1aa1c87ba442d7d2c6162e6e45fbb122d4133d
enriquelau <enriquelau@mac.com>
2021-03-09 16:36:27 -0600

    Removing the time method and using the sleep method created on line 23. Removing CC info.

--- targetsports_test.py
+++ targetsports_test.py
@@ -1,19 +1,16 @@
-import time
-from selenium import webdriver
+import from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException, StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
 from urllib3.exceptions import NewConnectionError
 from webdriver_manager import driver
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 # blazer_brass_124 = 'https://www.targetsportsusa.com/cci-blazer-brass-9mm-luger-ammo-124-grain-full-metal-jacket-5201-p-4172.aspx'
 quantity = "4"
 userEmail = "le1952@protonmail.com"
 userPass = "cZ2HyhARVft9h}Qt;$m"
 driver = webdriver.Chrome(ChromeDriverManager().install())
-cardMonth = "08"
-cardYear = "2025"
-cardSecurityCode = "8636"
 
 #Test website
 blazer_brass_124 = "https://www.targetsportsusa.com/federal-ae-357-sig-125-gr-ammo-fmj-ae357s2-p-1716.aspx"
@@ -58,37 +55,37 @@ def process_purchase():
         #Selects the 1000 round case option
         select = driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/div[1]/select/option[3]").click()
 
-        time.sleep(0.5)
+        sleep(0.5)
 
         #Enters the quantity to purchase
         search = driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[1]/input[1]")
         search.send_keys(Keys.BACK_SPACE)
-        time.sleep(0.2)
+        sleep(0.2)
         search.send_keys(quantity)
         search.send_keys(Keys.RETURN)
 
         #Adding product to the cart
-        time.sleep(0.5)
+        sleep(0.5)
         driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[3]/input").click()
 
         #Going to shopping cart
-        time.sleep(0.5)
+        sleep(0.5)
         driver.get("https://www.targetsportsusa.com/shoppingcart.aspx")
 
         #Clicking the Checkout Now button
-        time.sleep(0.5)
+        sleep(0.5)
         driver.find_element_by_xpath("/html/body/form/main/div/div/div[4]/div[2]/input[2]").click()
 
         #Logging in for checkout
-        time.sleep(0.5)
+        sleep(0.5)
         driver.find_element_by_class_name("form-control.signinEmail").send_keys(userEmail)
         driver.find_element_by_xpath("/html/body/form/main/div[1]/div/div/table/tbody/tr/td/div/div[1]/div[3]/input").send_keys(userPass)
 
-        time.sleep(0.5)
+        sleep(0.5)
         #Clicking the login button
         driver.find_element_by_class_name("button.call-to-action.login-button").click()
 
-        time.sleep(0.5)
+        sleep(0.5)
         #Button to place an order
         #driver.find_element_by_class_name("button.call-to-action.checkoutprocess-placeorder").click()
     except Exception as e:
