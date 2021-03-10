f8591afd82aad4157f547ba306208097020a357c
enriquelau <enriquelau@mac.com>
2021-03-09 19:31:50 -0600

    Laying added a function for the logic.

--- targetsports_test.py
+++ targetsports_test.py
@@ -7,17 +7,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # blazer_brass_124 = 'https://www.targetsportsusa.com/cci-blazer-brass-9mm-luger-ammo-124-grain-full-metal-jacket-5201-p-4172.aspx'
-quantity = "4"
+quantity = "1"
 userEmail = "le1952@protonmail.com"
 userPass = "cZ2HyhARVft9h}Qt;$m"
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-#Test website
+# Test website
 blazer_brass_124 = "https://www.targetsportsusa.com/federal-ae-357-sig-125-gr-ammo-fmj-ae357s2-p-1716.aspx"
-# blazer_brass_124 = 'https://www.targetsportsusa.com/cci-blazer-brass-9mm-luger-ammo-124-grain-full-metal-jacket-5201-p-4172.aspx'
-    #Trying to get the text that reads what is out of stock.
 
-    # Need to add a while loop to check in stock
+
+# blazer_brass_124 = 'https://www.targetsportsusa.com/cci-blazer-brass-9mm-luger-ammo-124-grain-full-metal-jacket-5201-p-4172.aspx'
+# Trying to get the text that reads what is out of stock.
+
+# Need to add a while loop to check in stock
 def sleep(secs):
     time.sleep(secs)
 
@@ -32,17 +34,31 @@ def check_status(get_status):
             print("line 35")
             return
 
+def inventory_check():
+    # Enters the quantity to purchase
+    search = driver.find_element_by_xpath(
+        "/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[1]/input[1]")
+    inventory = driver.find_element_by_css_selector("html body#product-page.master form#aspnetForm main div#ctl00_PageContent_pnlContent div div#product.product.common div.product-info div.product-options vinv.vinv_2629 div.product-stock div.stock-info")
+    print(inventory)
+    print(search)
+    search.send_keys(Keys.BACK_SPACE)
+    sleep(0.2)
+    search.send_keys(quantity)
+    search.send_keys(Keys.RETURN)
+    return
+
 
 def get_url():
     tries = 0
     while tries < 8:
         try:
             driver.get(blazer_brass_124)
-            #sleep(0.5)
-            get_status = driver.find_element_by_css_selector("#product > div.product-info > div.product-options > vinv.vinv_2628 > div > div.stock-info > span").text
+            # sleep(0.5)
+            get_status = driver.find_element_by_css_selector(
+                "#product > div.product-info > div.product-options > vinv.vinv_2628 > div > div.stock-info > span").text
             check_status(get_status)
             break
-        except Exception as e:#(NoSuchElementException,NewConnectionError,WebDriverException,ConnectionError,StaleElementReferenceException):
+        except Exception as e:  # (NoSuchElementException,NewConnectionError,WebDriverException,ConnectionError,StaleElementReferenceException):
             print(e)
             sleep(1)
             tries += 1
@@ -50,46 +66,55 @@ def get_url():
     process_purchase()
 
 
+
 def process_purchase():
     try:
-        #Selects the 1000 round case option
-        select = driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/div[1]/select/option[3]").click()
+        # Selects the 1000 round case option
+        select = driver.find_element_by_xpath(
+            "/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/div[1]/select/option[3]").click()
 
         sleep(0.5)
 
-        #Enters the quantity to purchase
-        search = driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[1]/input[1]")
+        # Enters the quantity to purchase
+
+        inventory_check()
+
+        search = driver.find_element_by_xpath(
+            "/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[1]/input[1]")
         search.send_keys(Keys.BACK_SPACE)
         sleep(0.2)
         search.send_keys(quantity)
         search.send_keys(Keys.RETURN)
 
-        #Adding product to the cart
+        # Adding product to the cart
         sleep(0.5)
-        driver.find_element_by_xpath("/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[3]/input").click()
+        driver.find_element_by_xpath(
+            "/html/body/form/main/div/div[1]/div[1]/div[3]/div[2]/vinv[2]/div/div[2]/div/span[3]/input").click()
 
-        #Going to shopping cart
+        # Going to shopping cart
         sleep(0.5)
         driver.get("https://www.targetsportsusa.com/shoppingcart.aspx")
 
-        #Clicking the Checkout Now button
+        # Clicking the Checkout Now button
         sleep(0.5)
         driver.find_element_by_xpath("/html/body/form/main/div/div/div[4]/div[2]/input[2]").click()
 
-        #Logging in for checkout
+        # Logging in for checkout
         sleep(0.5)
         driver.find_element_by_class_name("form-control.signinEmail").send_keys(userEmail)
-        driver.find_element_by_xpath("/html/body/form/main/div[1]/div/div/table/tbody/tr/td/div/div[1]/div[3]/input").send_keys(userPass)
+        driver.find_element_by_xpath(
+            "/html/body/form/main/div[1]/div/div/table/tbody/tr/td/div/div[1]/div[3]/input").send_keys(userPass)
 
         sleep(0.5)
-        #Clicking the login button
+        # Clicking the login button
         driver.find_element_by_class_name("button.call-to-action.login-button").click()
 
         sleep(0.5)
-        #Button to place an order
-        #driver.find_element_by_class_name("button.call-to-action.checkoutprocess-placeorder").click()
+        # Button to place an order
+        # driver.find_element_by_class_name("button.call-to-action.checkoutprocess-placeorder").click()
     except Exception as e:
         print(e)
         pass
+
 
 get_url()
