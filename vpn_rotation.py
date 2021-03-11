de58943a95f7d1693b11f4f9341b71b01be1094b
Enrique Lau <enriquelau@mac.com>
2021-03-10 16:53:30 -0800

    Committing vpn_rotation changes.

---
+++ vpn_rotation.py
@@ -0,0 +1,41 @@
+import subprocess
+import csv
+import pandas as pd
+
+
+
+
+
+df = pd.read_csv(r'~/web_requests/vpn_server_names.csv')
+
+print(df)
+
+
+
+
+
+
+'''
+
+with open ('vpn_server_names.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    print(csv_reader)
+'''
+
+
+
+'''
+profiles = 
+for i in profiles:
+
+
+subprocess.call(['ls','-l'], shell=True)
+'''
+'''
+sudo openvpn us.protonvpn.com.udp.ovpn
+
+The above command is how to use openvpn to connect to a 
+vpn server
+
+
+'''
