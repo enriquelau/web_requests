fd26584e1ba757e8ef6a6c433c5248d5badc9b1e
enriquelau <enriquelau@mac.com>
2021-03-23 13:58:24 -0500

    Updating name tempt to page

--- requests_learning.py
+++ requests_learning.py
@@ -2,15 +2,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 session = requests.Session()
-
-
-def add_to_cart():
-    headers = {
+headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/83.0.4103.116 Safari/537.36"}
+
+def add_to_cart():
     global session
-    endpoint = "https://www.speer.com/on/demandware.store/Sites-VistaSpeer-Site/default/Product-Inventory?pid=19-53919&qty=1"
-    response = session.get(endpoint)
+    endpoint = "https://www.speer.com/ammunition/handgun/lawman_handgun_training/19-53919.html"
+    payload = {
+        "pid":"19-53919",
+        "quantity":"3",
+        "options":'%%5B%%5D'
+    }
+    response = session.post(endpoint, data=payload)
+
     print (response.text)
 add_to_cart()
 
