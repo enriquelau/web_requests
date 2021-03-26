aec215d5a91c3e555d143acb4a27f26f5e02273a
enriquelau <enriquelau@mac.com>
2021-03-26 01:40:59 -0500

    Notes in temp.py left for next steps.

--- temp.py
+++ temp.py
@@ -1,22 +1,46 @@
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 
+'''
+Today's lesson: 
 
+1. Need to store the cookie sessions to make the session persist for checkout. 
+2. Need to implement a way to hold the csrf token, which will require a function 
+   that will obtain it and keep it alive for the checkout process. 
+
+'''
+url = 'https://www.speer.com/bullets/handgun_bullets/gold_dot_handgun_component_bullet/19-3985.html'
 session = requests.Session()
-url = 'https://www.speer.com/on/demandware.store/Sites-VistaSpeer-Site/default/Cart-AddProduct'
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/83.0.4103.116 Safari/537.36"}
-payload = {"pid":"19-53919","quantity":"3","stateCode":"TX"}
-add_to_cart = requests.post(url,headers=headers,data=payload)
 
-# Will need to add ship to state
+
+# This will be the function that will save initial cookie, possibly?
+# May need another one for the csrf cookie session. 
+def initiate():
+    global session
+    response = session.get(url)
+    print(response.text)
+
+
+def add_to_cart():
+    return
+    #global session 
+    #adding to cart with the below post request
+    #payload = {"pid":"19-53919","quantity":"3"}
+    #requests.post(url,headers=headers,data=payload)
+    #print(initiate(response))
+
+'''
+soup = bs(response.text,"html.parser")
+r = requests.get("https://www.speer.com/on/demandware.store/Sites-VistaSpeer-Site/default/Cart-CheckShippingRestrictions?stateCode=TX")
+'''
+
+# account_creds = {}
 
 
 # Login for checkout
-
-
-
 
 '''
 It seems to work, although I would like another way to verify. 
@@ -26,11 +50,6 @@ This is only for testing.
 '''
 # checkState = ''
 
-print(add_to_cart.content)
-
-
-
-
 
 #def add_to_cart():
-    
+initiate() 
