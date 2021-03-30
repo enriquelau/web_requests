9ca73756e01ed259919990c18dfaf021ccd872e9
enriquelau <enriquelau@mac.com>
2021-03-30 01:01:38 -0500

    I got text using BS4, more to come.

--- ammoseek9mm.py
+++ ammoseek9mm.py
@@ -1,27 +1,27 @@
 import requests
+import lxml
 from bs4 import BeautifulSoup as bs
 import RandomHeaders
 
-url = 'https://ammoseek.com/ammo/9mm-luger'
-
+URL = 'https://ammoseek.com/ammo/9mm-luger'
 
 def headers():
     header = RandomHeaders.LoadHeader() 
     return header
 
+def get_page_url(URL):
+    res = requests.get(URL, headers=headers())
+    return res.content
 
-def get_page_html(url):
-    page = requests.get(url, headers=headers())
-    return 
-
-def price():
-    soup = bs(get_page_html(url), 'html.parser')
-    price_check = soup.find("div", {"class": "results-info-content"})
-    return price_check
-
+def parcing_price(page_html):
+    soup = bs(page_html, 'html.parser')
+    price = soup.find_all("div", {"class": "results-info-content"})
+    print(price)
+    return price
 
 def check_price():
-    page_html = get_page_html(url)
-    print(price())
-    
+    page_html = get_page_url(URL)
+    temp = parcing_price(page_html)
+    print('temp')
+
 check_price()
