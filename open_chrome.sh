43bde2a26a9b4ada4dba1755f5243dec8d39cc30
enriquelau <enriquelau@mac.com>
2021-05-22 19:56:55 -0500

    Adding two scripts for running scripts on schedule

---
+++ open_chrome.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# python3 /Users/enriquelau/web_requests/requests_file.py
+open /Applications/Google\ Chrome.app
---
+++ speer115.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 /Users/enriquelau/web_requests/requests_file.py https://www.speer.com/ammunition/handgun/lawman_handgun_cleanfire_training/19-53826.html 5 40
+# open /Applications/Brave\ Browser.app 
